51908345e6ac8c3b4f4598348e6715f26d906ae0
monkeygobblerkenny <kennyxia2001@gmail.com>
2022-12-13 16:43:03 -0500

    CSS stuff
    Login page and nav bar icons

--- pages/Login.js
+++ pages/Login.js
@@ -1,8 +1,9 @@
-import { Button, View, Text, TextInput } from 'react-native';
+import { Button, View, Text, TextInput, Image } from 'react-native';
 import React, { useState } from 'react'
 import axios from 'axios'
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { RefreshControl, SafeAreaView, StyleSheet,ScrollView } from 'react-native';
+import Toast from 'react-native-toast-message'
 
 function Login({ navigation }) {
     const [USER_EMAIL, setUSER_EMAIL] = useState('')
@@ -26,6 +27,7 @@ function Login({ navigation }) {
 
 
     const signin = () => {
+      Toast.show({type:'info', text1: 'Logging in...', position:'top', topOffset:0})
       let url = API_BASE_URL + API_SIGN_IN_URL
       axios.post(url,
         {
@@ -35,45 +37,53 @@ function Login({ navigation }) {
         .then(async function (response) {
           //setJWT_TOKEN(JSON.stringify(response.data.token))
           AsyncStorage.setItem('JWT_TOKEN', JSON.stringify(response.data.token))
-          alert('done')
+          //alert('done')
           const jsonValue = await AsyncStorage.getItem('JWT_TOKEN')
-          alert(jsonValue != null ? JSON.parse(jsonValue) : null)
+          //alert(jsonValue != null ? JSON.parse(jsonValue) : null)
           AsyncStorage.setItem('contractor', JSON.stringify(response.data.contractor))
           console.log('Are you a contractor? '+ await AsyncStorage.getItem('contractor'))
           AsyncStorage.setItem('image', JSON.stringify(response.data.image))
           //const jsonValue = JSON.stringify(response.data.token)
+          Toast.show({type:'success',text1: 'Login Successful'})
         }).catch(function (error) {
           console.log(error.response.status)
+          Toast.show({type:'error',text1:'Invalid Credentials'})
         })
-
         
     }
 
       
 
     return (
+      
       <ScrollView
           refreshControl={
             <RefreshControl
               refreshing = {refreshing}
               onRefresh={onRefresh}
            />
-          }
+          } keyboardShouldPersistTaps={'handled'}
         >
+      <View style={{backgroundColor:'white', maxHeight:175}}>
+        <Text style={{fontSize: 20,textAlign: 'center',backgroundColor:'white',paddingBottom:20}}>WELCOME TO WORKSPACE</Text>
+        <Image source={require('../assets/icon.png')} style={{maxHeight:100,maxWidth:100,display:'flex',alignContent:'center',alignSelf:'center',backgroundColor:'white'}}></Image>
+        <Toast />
+      </View>
       <View>
         <TextInput 
           placeholder="Email" 
-          onChangeText={(USER_EMAIL) => setUSER_EMAIL(USER_EMAIL)}
-          />
-          
+          onChangeText={(USER_EMAIL) => setUSER_EMAIL(USER_EMAIL)} keyboardShouldPersistTaps={'handled'}
+          /> 
+        <Text style={{backgroundColor:'white', fontSize:1}}></Text>
         <TextInput
           secureTextEntry={true}
           placeholder="Password"
-          onChangeText={(USER_PASSWORD) => setUSER_PASSWORD(USER_PASSWORD)}
+          onChangeText={(USER_PASSWORD) => setUSER_PASSWORD(USER_PASSWORD)} keyboardShouldPersistTaps={'handled'}
         />
-        <Button color='black' title='Sign in' onPress={signin} />
-    </View>
+        <Text style={{backgroundColor:'white', fontSize:1}}></Text>
+        <Button color='black' title='Sign in' onPress={signin} style={{padding:50}}/>
     
+    </View>
     </ScrollView>
     );
   }
